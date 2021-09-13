b66eb7965ce7a052411651b244b4cf142606a4bc
Ahmed Moazzam <muhammedahmedmoazzam@gmail.com>
2021-09-12 22:26:25 -0400

    Scraping Footlocker almost done.

--- scraping/scrape_footlocker.py
+++ scraping/scrape_footlocker.py
@@ -3,10 +3,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 from bs4 import BeautifulSoup
 from time import sleep
 import requests
+import json
 
 driver = webdriver.Chrome(executable_path='../../../../Webdriver/bin/chromedriver')
 
@@ -16,36 +18,86 @@ categories = {
              # "kids" : "https://www.footlocker.ca/en/category/kids/shoes.html"
 }
 
-def scrape_shoe(url):
+shoe_list = []
+
+def last_page(driver, html):
+    soup = BeautifulSoup(html, 'lxml')
+    soup = soup.find('ul', 'row row--always gutterH')
+    print(soup)
+    buttons = soup.children
+
+    count = 0
+    for button in buttons:
+        count += 1
+
+    next_page_button = '//*[@id="main"]/div/div[2]/div/section/div/div[2]/nav/ul/li[' + str(count-1) + ']/a'
+
+    try:
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, next_page_button))).click()
+    except (NoSuchElementException, TimeoutException) as e:
+        return True
+
+    return False
+
+def write_to_json(file_data, filename='outfile.json'):
+    with open(filename, 'r+') as file:
+        json.dump(file_data, file, indent = 4)
+
+def scrape_shoe(url, name, colours):
     res = requests.get(url)
 
     soup = BeautifulSoup(res.text, 'lxml')
     try:
-        data = soup.find_all('script', type='application/ld+json')[1]
+        data = str(soup.find_all('script', type='application/ld+json')[1])[42:-15]
     except IndexError:
         return
 
-    print(data)
+    json_data = json.loads(data)
 
+    try:
+        brand = json_data['brand']
+    except KeyError:
+        brand = name.split()[0]
+
+    shoe_obj = {
+        'name' : name,
+        'colours' : colours,
+        'description' : json_data['description'],
+        'image' : json_data['image'],
+        'brand' : brand,
+        'model' : json_data['model'],
+        'sku' : json_data['sku'],
+        'condition' : json_data['itemCondition'],
+        'url' : json_data['@id'],
+        'offers' : json_data['offers']
+    }
+    shoe_list.append(shoe_obj)
 
 def scrape_category(category: str):
     driver.get(categories.get(category))
-    html = driver.page_source
 
-    soup = BeautifulSoup(html, 'lxml')
-    shoes = soup.find_all('li', 'product-container col')
+    while True:
+        html = driver.page_source
 
-    for shoe in shoes:
-        print(shoe.find('span', 'ProductName-primary').text)
-        links = [a['href'] for a in shoe.select('a[href]') if a.text]
+        soup = BeautifulSoup(html, 'lxml')
+        shoes = soup.find_all('li', 'product-container col')
 
-        for link in links:
-            url = "https://www.footlocker.ca" + link
-            scrape_shoe(url)
+        for shoe in shoes:
+            name = shoe.find('span', 'ProductName-primary').text
+            print(name)
+            colours = shoe.find('span', 'ProductName-alt').text[6:].split('/')
+            links = [a['href'] for a in shoe.select('a[href]') if a.text]
+
+            for link in links:
+                url = "https://www.footlocker.ca" + link
+                scrape_shoe(url, name, colours)
+        if last_page(driver, html):
+            write_to_json(shoe_list)
+            break
 
 def main():
     for category in categories.keys():
-        print(category)
+        print("Scraping " + category + "...")
         scrape_category(category)
 
 main()
